6bc3c20e62ab6956b4e10b914a59943dd9c6fbd6
VivianC <viviancao58@gmail.com>
2022-11-16 16:54:23 +1100

    create food list item component

---
+++ src/components/ListItem/index.jsx
@@ -0,0 +1,22 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUtensils } from '@fortawesome/free-solid-svg-icons'
+
+function FoodListItem(props) {
+    return (
+        <li>
+            <h3>
+                {props.foodData.title} - {props.foodData.rating}/5
+                {props.foodData.haveTried && (<FontAwesomeIcon icon={faUtensils}/>)}
+            </h3>
+                
+            <img src={props.foodData.image} alt={props.foodData.title} />
+            <p>
+                {props.foodData.description}
+            </p>
+
+
+        </li>
+    )
+}
+
+export default FoodListItem
