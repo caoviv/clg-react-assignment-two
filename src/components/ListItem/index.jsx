379b3cf3638009aca169c63f6b711821c3ff64a4
VivianC <viviancao58@gmail.com>
2022-11-20 14:19:02 +1100

    add rating to fooditem componant

--- src/components/ListItem/index.jsx
+++ src/components/ListItem/index.jsx
@@ -1,19 +1,20 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUtensils } from '@fortawesome/free-solid-svg-icons'
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUtensils } from "@fortawesome/free-solid-svg-icons";
 
 function FoodListItem(props) {
-    return (
-        <li>
-            <h3>
-                {props.foodData.title} - {props.foodData.haveTried && (<FontAwesomeIcon icon={faUtensils}/>)} {props.foodData.rating === 0 ? 'No Rating' : `${props.foodData.rating}/5`} 
-            </h3>                
-            <img src={props.foodData.image} alt={props.foodData.title} />
-            <p>
-                {props.foodData.description}
-            </p>
-        </li>
-    )
+  return (
+    <li>
+      <h3>
+        {props.foodData.title} -{" "}
+        {props.foodData.haveTried && <FontAwesomeIcon icon={faUtensils} />}{" "}
+        {props.foodData.rating === 0
+          ? "No Rating"
+          : `${props.foodData.rating}/5`}
+      </h3>
+      <img src={props.foodData.image} alt={props.foodData.title} />
+      <p>{props.foodData.description}</p>
+    </li>
+  );
 }
 
-export default FoodListItem
+export default FoodListItem;
